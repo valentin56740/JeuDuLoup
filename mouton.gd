7e038671f9ced39471bac6c665b4aca3ff1212e4
valentin.enaud <valentin.enaud@depinfoad.univ-brest.fr>
2025-09-29 17:08:56 +0200

    Mouvement aléatoire des moutons

--- mouton.gd
+++ mouton.gd
@@ -1,14 +1,26 @@
 extends RigidBody3D
 
+@export var speed: float = 20
+@export var direction_change_interval: float = 2.0 # toutes les 2 sec environ
 
-@export var force = 15
+var move_dir: Vector3 = Vector3.ZERO
+var timer: float = 0.0
 
-var sx:float = 50
-var sz:float = 50 
+var sx:float = 100
+var sz:float = 100 
 
 func _process(delta:float) -> void:
-	var fz = randf()-0.5 
-	apply_central_force( Vector3(force,0,fz))
+	# décrémente le timer
+	timer -= delta
+	
+	if timer <= 0.0:
+		# choisit une nouvelle direction aléatoire
+		var angle = randf() * TAU  # angle aléatoire [0..2π] 
+		move_dir = Vector3(cos(angle), 0, sin(angle)).normalized() # Vecteur directionnel du mouton 
+		timer = direction_change_interval
+
+	# applique une force dans la direction choisie
+	apply_central_force(move_dir * speed)
 	
 	if position.x > sx/2:
 		position.x =-sx/2
