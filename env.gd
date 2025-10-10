a4ca779fcfa4f035d325155e9ddd9b3a8ab0a770
valentin.enaud <valentin.enaud@depinfoad.univ-brest.fr>
2025-10-10 09:42:30 +0200

    Mise à jour

--- env.gd
+++ env.gd
@@ -1,6 +1,16 @@
 extends Node3D
 
+
+@export var cameras : Array[Camera3D] = [] 
+var index = 0 
 var mouton = preload("res://mouton.tscn")
+
+func _ready() -> void:
+	update_camera()
+
+func update_camera():
+	for i in range(cameras.size()):
+		cameras[i].current = (i==index)
 
 #func _init() -> void: 
 	#for i in range(100):   
@@ -11,3 +21,8 @@ var mouton = preload("res://mouton.tscn")
 		#m.add_to_group("Mouton")
 		#
 		#add_child(m)  
+
+
+func _on_button_pressed() -> void:
+	index = (index+1)%cameras.size()
+	update_camera() 
